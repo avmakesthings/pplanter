36f2185a6d91a62398af7039f68a404baef7b3a2
John Victor-Faichney <john.victor.faichney@gmail.com>
2015-08-03 23:13:09 -0700

    added missing dependencies, updated gulpfile and paths

--- gulpfile.js
+++ gulpfile.js
@@ -56,12 +56,24 @@ gulp.task('copy', function() {
   gulp.src('./src/img/**/*.*')
     .pipe(gulp.dest('./dist/img'));
   gulp.src('./src/fonts/**/*.*')
-    .pipe(gulp.dest('./dist/fonts'));    
-  gulp.src('./bower_components/bootstrap/fonts/**/*.*')
     .pipe(gulp.dest('./dist/fonts'));
-  gulp.src('./bower_components/bootstrap/dist/js/*')
+  gulp.src('./bower_components/bootstrap/fonts/**/*.*')
+    .pipe(gulp.dest('./dist/fonts'));  
+  gulp.src('./bower_components/fontawesome/fonts/**/*.*')
+    .pipe(gulp.dest('./dist/fonts')); 
+  gulp.src('./bower_components/fontawesome/css/font-awesome.min.css')
+    .pipe(gulp.dest('./dist/css'));
+  gulp.src('./bower_components/bootstrap/dist/css/bootstrap.min.css')
+    .pipe(gulp.dest('./dist/css'));  
+  gulp.src('./bower_components/bootstrap/dist/js/bootstrap.min.js')
     .pipe(gulp.dest('./dist/js'));
-  gulp.src('./bower_components/jquery/dist/*')
+  gulp.src('./bower_components/jquery/dist/jquery.min.js')
+    .pipe(gulp.dest('./dist/js'));
+  gulp.src('./bower_components/snap.svg/dist/snap.svg-min.js')
+    .pipe(gulp.dest('./dist/js'));
+  gulp.src('./bower_components/html5shiv/dist/html5shiv.min.js')
+    .pipe(gulp.dest('./dist/js'));
+  gulp.src('./bower_components/respond/dest/respond.min.js')
     .pipe(gulp.dest('./dist/js'));
 });
 
